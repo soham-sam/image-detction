fab42d44470c03f553267e6881993963c492e630
soham-sam <85211902+soham-sam@users.noreply.github.com>
2022-02-13 13:30:18 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function bedroom() {
+    window.location="bed_room.html"
+}
+
+function desk() {
+    window.location="desk.html"
+}
+
+function fruit() {
+    window.location="fruit.html"
+}
+
+function bottle() {
+    window.location="bottle.html"
+}
